0df700e2e6f048491ebcefeb75d54b8d5ddf11ac
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-04-06 22:16:26 -0600

    Crear archivo separado para clase Estudiante

---
+++ src/estudiante.ts
@@ -0,0 +1,13 @@
+class Estudiante {
+
+	nombre:string;
+	apellidoPaterno:string;
+	apellidoMaterno:string;
+
+	constructor( nombre, apellidoPaterno, apellidoMaterno ) {
+		this.nombre = nombre;
+		this.apellidoPaterno = apellidoPaterno;
+		this.apellidoMaterno = apellidoMaterno;
+	}
+
+}
