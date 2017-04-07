a5b97f51b6041a18e5a5d0510e059d319a7b7103
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-04-06 22:14:44 -0600

    Agregar propiedades a una clase en TypeScript

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,13 @@
 class Estudiante {
 
+	nombre;
+	apellidoPaterno;
+	apellidoMaterno;
+
+	constructor( nombre, apellidoPaterno, apellidoMaterno ) {
+		this.nombre = nombre;
+		this.apellidoPaterno = apellidoPaterno;
+		this.apellidoMaterno = apellidoMaterno;
+	}
 
 }
